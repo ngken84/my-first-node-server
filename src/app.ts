640053c1f22228769f932848e6daadd3e677b95d
Ken Ng <ng.ken84@gmail.com>
2024-11-30 19:51:05 -0800

    Add a form and behavior on sent

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,32 @@
 import * as http from 'http';
+import * as fs from 'fs';
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method, req.headers);
+    const url = req.url;
+
+    if(url === "/") {
+        res.setHeader("Content-Type", "text/html");
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text"/><button type="submit">Send</button></form></body>')
+        res.write('</html>');
+        return res.end();
+    }
+    if(url === "/message" && req.method === 'POST') {
+        fs.writeFileSync("message.txt", "DUMMY");
+        res.statusCode = 302;
+        res.setHeader('Location', "/");
+        return res.end();
+    }
     res.setHeader("Content-Type", "text/html");
     res.write('<html>');
     res.write('<head></head>');
     res.write('<body>Hello World</body>')
     res.write('</html>');
-    res.end();
+    return res.end();
+
+    
 });
 
 server.listen(3000);
