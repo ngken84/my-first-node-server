ebdac6baa2741c6f1a2bce0b259a71fa002201bc
Ken Ng <ng.ken84@gmail.com>
2024-11-30 19:27:29 -0800

    Add a response to server

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,12 @@ import * as http from 'http';
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method, req.headers);
+    res.setHeader("Content-Type", "text/html");
+    res.write('<html>');
+    res.write('<head></head>');
+    res.write('<body>Hello World</body>')
+    res.write('</html>');
+    res.end();
 });
 
 server.listen(3000);
